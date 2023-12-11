1ab9dec8380b6735fa20ab967690d62b36983c40
Sang Phạm <youlovelong123@gmail.com>
2023-12-11 18:03:49 +0700

    [Add] SomeStuff

--- lib/common/routes/routes.dart
+++ lib/common/routes/routes.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:beautiful_diary_app/core/di/service_locator.dart';
+import 'package:page_transition/page_transition.dart';
 
 import '../../src/dashboard/blocs/main_bloc.dart';
 import '../../src/dashboard/presentation/screens/main_screen.dart';
+import '../../src/dashboard/presentation/screens/notch_bottom_wrapper.dart';
 
 class AppRouter {
   GoRouter generateRoute() {
@@ -16,7 +19,19 @@ class AppRouter {
             child: const MainScreen(),
           );
         },
-      )
+      ),
+      GoRoute(
+        path: '/notchbar',
+        builder: (context, state) {
+          Navigator.push(
+            context,
+            PageTransition(
+                type: PageTransitionType.leftToRight,
+                child: const AnimatedNotchBottomBarWrapper()),
+          );
+          return Container();
+        },
+      ),
     ]);
   }
 }
